a7d251cc2296e25840075ccb303feff4eca5cb45
Wido den Hollander <wido@widodh.nl>
2016-04-13 17:08:05 +0200

    Enable IPv6 by default

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
         graphiteHost: process.env.GRAPHITE_HOST || "localhost",
         port: parseInt(process.env.STATSD_PORT) || 8125,
         mgmt_port: parseInt(process.env.STATSD_MANAGEMENT_PORT) || 8126,
-        address_ipv6: JSON.parse(process.env.STATSD_IPV6) || true
+        address_ipv6: true
     };
 })();
